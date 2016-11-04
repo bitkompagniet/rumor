3d19bc605f4e18502ea6caf7f3dc65c8c6e8451d
Kristian Videmark Parkov <parkov@bitkompagniet.dk>
2016-11-04 15:51:57 +0100

    Update example with catch

--- examples/promise.js
+++ examples/promise.js
@@ -8,5 +8,6 @@ Promise.resolve({ x: 3, name: 'hest', deep: { obj: { with: { a: { lot: { of: 'ke
 
 Promise.resolve(['this', 'is', 'a', 'test'])
 	.then(rumor.info)
-	.then(rumor.warn)
-	.then(rumor.error);
+	.then(() => { throw new Error('This is all wrong!'); })
+	.then(() => rumor.info('This will never be reached'))
+	.catch(rumor.error);
